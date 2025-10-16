9f6489aebdd511b9005af8d4d5f788f9e0d0b773
Teimurazi Baindurashvili <tbaindurashvili@credo.ge>
2025-10-17 00:25:08 +0400

    Modify wildcards_handler.c according to norminette

--- minishell/wildcards_handler.c
+++ minishell/wildcards_handler.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcards_handler.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbaindur <tbaindur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 00:00:00 by tbaindur          #+#    #+#             */
+/*   Updated: 2025/10/17 00:00:00 by tbaindur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell_types.h"
 #include <dirent.h>
 #include <stdlib.h>
@@ -12,86 +24,117 @@ static void	add_token(t_token **head, t_token **tail, t_token *new_tok)
 	*tail = new_tok;
 }
 
-// Single '*' support, prefix*suffix match
 static int	match_star(const char *pattern, const char *s)
 {
 	const char	*star;
-	char		before[256] = {0};
-	char		after[256] = {0};
 	size_t		blen;
 	size_t		alen;
-	size_t		slen;
+	size_t		s_len;
 
 	star = strchr(pattern, '*');
 	if (!star)
 		return (strcmp(pattern, s) == 0);
-	strncpy(before, pattern, (size_t)(star - pattern));
-	strcpy(after, star + 1);
-	blen = strlen(before);
-	alen = strlen(after);
-	slen = strlen(s);
-	if (blen && strncmp(s, before, blen) != 0)
+	blen = (size_t)(star - pattern);
+	alen = strlen(star + 1);
+	s_len = strlen(s);
+	if (blen && (s_len < blen || strncmp(s, pattern, blen) != 0))
 		return (0);
-	if (alen && (slen < alen || strcmp(s + slen - alen, after) != 0))
+	if (alen && (s_len < alen || strcmp(s + (s_len - alen), star + 1) != 0))
 		return (0);
-	if (slen < blen + alen)
+	if (s_len < blen + alen)
 		return (0);
 	return (1);
 }
 
+static int	should_skip_name(const char *name, int pat_starts_dot)
+{
+	if (!pat_starts_dot && name[0] == '.')
+		return (1);
+	if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0)
+		return (1);
+	return (0);
+}
+
+static int	expand_pattern_from_cwd(const char *pattern, int pat_starts_dot,
+			t_token **new_head, t_token **new_tail)
+{
+	DIR				*dir;
+	struct dirent	*entry;
+	int				matched;
+
+	dir = opendir(".");
+	if (!dir)
+		return (0);
+	matched = 0;
+	entry = readdir(dir);
+	while (entry)
+	{
+		if (!should_skip_name(entry->d_name, pat_starts_dot)
+			&& match_star(pattern, entry->d_name))
+		{
+			add_token(new_head, new_tail, new_token(TOKEN_WORD,
+					strdup(entry->d_name), QUOTE_NONE));
+			matched = 1;
+		}
+		entry = readdir(dir);
+	}
+	closedir(dir);
+	return (matched);
+}
+
+static void	process_wildcard_token(t_token *cur, t_token **new_head,
+				t_token **new_tail)
+{
+	int	pat_starts_dot;
+	int	matched;
+
+	if (!cur->value)
+		return ;
+	pat_starts_dot = (cur->value[0] == '.');
+	matched = expand_pattern_from_cwd(cur->value, pat_starts_dot,
+			new_head, new_tail);
+	if (!matched)
+		add_token(new_head, new_tail, new_token(TOKEN_WORD,
+				strdup(cur->value), QUOTE_NONE));
+}
+
+static void	free_old_tokens(t_token *tokens)
+{
+	t_token	*tmp;
+
+	while (tokens)
+	{
+		tmp = tokens;
+		tokens = tokens->next;
+		free(tmp->value);
+		free(tmp);
+	}
+}
+
 t_token	*expand_wildcards(t_token *tokens)
 {
-	t_token *new_head;
-	t_token *new_tail;
-	t_token *cur;
+	t_token	*new_head;
+	t_token	*new_tail;
+	t_token	*cur;
 
 	new_head = NULL;
 	new_tail = NULL;
 	cur = tokens;
 	while (cur)
 	{
-		if (cur->type == TOKEN_WORD && strchr(cur->value, '*'))
-		{
-			DIR *dir;
-			struct dirent *entry;
-			int matched;
-			int pat_starts_dot;
-
-			dir = opendir(".");
-			if (!dir)
-			{
-				add_token(&new_head, &new_tail, new_token(TOKEN_WORD,
-						strdup(cur->value), QUOTE_NONE));
-				cur = cur->next;
-				continue ;
-			}
-			matched = 0;
-			pat_starts_dot = (cur->value[0] == '.');
-			while ((entry = readdir(dir)))
-			{
-				if (!pat_starts_dot && entry->d_name[0] == '.')
-					continue ;
-				if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name,
-						"..") == 0)
-					continue ;
-				if (match_star(cur->value, entry->d_name))
-				{
-					add_token(&new_head, &new_tail, new_token(TOKEN_WORD,
-							strdup(entry->d_name), QUOTE_NONE));
-					matched = 1;
-				}
-			}
-			closedir(dir);
-			if (!matched)
-				add_token(&new_head, &new_tail, new_token(TOKEN_WORD,
-						strdup(cur->value), QUOTE_NONE));
-		}
+		if (cur->type == TOKEN_WORD && cur->value && strchr(cur->value, '*'))
+			process_wildcard_token(cur, &new_head, &new_tail);
 		else
 		{
-			add_token(&new_head, &new_tail, new_token(cur->type,
-					strdup(cur->value), cur->quote));
+			if (cur->value)
+				add_token(&new_head, &new_tail, new_token(cur->type,
+						strdup(cur->value), cur->quote));
+			else
+				add_token(&new_head, &new_tail, new_token(cur->type,
+						NULL, cur->quote));
 		}
 		cur = cur->next;
 	}
+	free_old_tokens(tokens);
 	return (new_head);
 }
